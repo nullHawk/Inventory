fba349f06c4a55bd89d5a34bf7f9d299899b0e63
nullhawk <suryanshsinghshakya1@gmail.com>
2024-04-29 16:37:37 +0530

    intialized springboot project

---
+++ src/test/java/com/nullhawk/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nullhawk.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
