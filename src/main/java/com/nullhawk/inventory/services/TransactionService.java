f508c3e825207f5f4054818f0d0a4303b4411ea1
Venki1402 <141071215+Venki1402@users.noreply.github.com>
2024-05-01 16:48:46 +0530

    bug fix

--- src/main/java/com/nullhawk/inventory/services/TransactionService.java
+++ src/main/java/com/nullhawk/inventory/services/TransactionService.java
@@ -1,8 +1,12 @@
 package com.nullhawk.inventory.services;
 
+import com.nullhawk.inventory.exceptions.TransactionException;
+import com.nullhawk.inventory.models.Transaction;
 import com.nullhawk.inventory.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TransactionService {
@@ -13,29 +17,29 @@ public class TransactionService {
         this.transactionRepository = transactionRepository;
     }
 
-//    public List<Transaction> getAllTransactions() {
-//        return transactionRepository.findAll();
-//    }
-//
-//    public Transaction getTransaction(Long id) {
-//        return transactionRepository.findById(id).orElseThrow(() -> new TransactionException("Transaction not found"));
-//    }
-//
-//    public Transaction createTransaction(Transaction transaction) {
-//        return transactionRepository.save(transaction);
-//    }
-//
-//    public Transaction updateTransaction(Long id, Transaction transactionDetails) {
-//        Transaction transaction = getTransaction(id);
-//        transaction.setItemId(transactionDetails.getItemId());
-//        transaction.setTransactionType(transactionDetails.getTransactionType());
-//        transaction.setQuantity(transactionDetails.getQuantity());
-//        transaction.setTransactionDate(transactionDetails.getTransactionDate());
-//        return transactionRepository.save(transaction);
-//    }
-//
-//    public void deleteTransaction(Long id) {
-//        Transaction transaction = getTransaction(id);
-//        transactionRepository.delete(transaction);
-//    }
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction getTransaction(Long id) {
+        return transactionRepository.findById(id).orElseThrow(() -> new TransactionException("Transaction not found"));
+    }
+
+    public Transaction createTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public Transaction updateTransaction(Long id, Transaction transactionDetails) {
+        Transaction transaction = getTransaction(id);
+        transaction.setTransactionId(transactionDetails.getTransactionId());
+        transaction.setTransactionType(transactionDetails.getTransactionType());
+        transaction.setQuantity(transactionDetails.getQuantity());
+        transaction.setTransactionDate(transactionDetails.getTransactionDate());
+        return transactionRepository.save(transaction);
+    }
+
+    public void deleteTransaction(Long id) {
+        Transaction transaction = getTransaction(id);
+        transactionRepository.delete(transaction);
+    }
 }
